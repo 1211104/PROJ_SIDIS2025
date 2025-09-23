122ec9152d9b76341f34bd4fcd8b77f6ee18bdde
José Neto <1211104@isep.ipp.pt>
2025-09-23 15:20:52 +0100

    Initial commit

---
+++ src/main/java/org/example/proj_sidis2025/ProjSidis2025Application.java
@@ -0,0 +1,13 @@
+package org.example.proj_sidis2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjSidis2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjSidis2025Application.class, args);
+	}
+
+}
