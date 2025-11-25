8851ef58cb47dbc374bcaa7babb1094506d2408b
FilipeCardosoFac <1201286@isep.ipp.pt>
2025-11-25 15:01:11 +0000

    data.sql delete

--- docs/data-replicaB.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM physician;
-
-INSERT INTO physician (physician_number, name, specialty, contact_info) VALUES
-    ('PH101','César Oliveira','Pele','cesar.oliveira@hap.xyz'),
-    ('PH102','Josefa Bacalhau','Dentes','josefa.bacalhau@hap.xyz');
--- docs/data.sql
+++
@@ -1,6 +0,0 @@
--- Limpeza defensiva (ignorado se vazio)
-DELETE FROM physician;
-
-INSERT INTO physician (physician_number, name, specialty, contact_info) VALUES
-    ('PH001','Carlos Albergaria','Ossos','carlos.albergaria@hap.xyz'),
-    ('PH002','Ana Fonseca','Olhos','ana.fonseca@hap.xyz');
